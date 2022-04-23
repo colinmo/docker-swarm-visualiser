8ed008cc667815eddd1b456aa092e9fdeabe7e69
Colin Morris <relapse@gmail.com>
2022-04-23 11:51:01 +1000

    Moved test to the better location

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"docker-swarm-visualiser/cmd"
-	"docker-swarm-visualiser/utils/mocks"
 	"log"
 
 	"fyne.io/fyne/v2"
@@ -31,24 +30,8 @@ var MainApp fyne.App
 var MainWindow fyne.Window
 var ActiveWindows map[string]fyne.Window
 
-func testMode() {
-	// TESTING
-	mocks.PatchDockerForTesting(&Docker)
-	mocks.AddCommandLines([]mocks.CommandStruct{
-		// List context
-		{Out: []byte(`[{"Current":true,"Description":"Current DOCKER_HOST based configuration","DockerEndpoint":"npipe:////./pipe/docker_engine","KubernetesEndpoint":"","ContextType":"moby","Name":"default","StackOrchestrator":"swarm"},{"Current":false,"Description":"","DockerEndpoint":"npipe:////./pipe/dockerDesktopLinuxEngine","KubernetesEndpoint":"","ContextType":"moby","Name":"desktop-linux","StackOrchestrator":""}]`), Err: nil},
-		// Set context
-		{Out: []byte(`[{"Current":true,"Description":"Current DOCKER_HOST based configuration","DockerEndpoint":"npipe:////./pipe/docker_engine","KubernetesEndpoint":"","ContextType":"moby","Name":"default","StackOrchestrator":"swarm"},{"Current":false,"Description":"","DockerEndpoint":"npipe:////./pipe/dockerDesktopLinuxEngine","KubernetesEndpoint":"","ContextType":"moby","Name":"desktop-linux","StackOrchestrator":""}]`), Err: nil},
-		// List services
-		{Out: []byte("36xvvwwauej0|~|frontend|~|replicated|~|5/5|~|nginx:alpine|~|80\n74nzcxxjv6fq|~|backend|~|replicated|~|3/3|~|redis:3.0.6|~|443\n"), Err: nil},
-		// Reset context
-		{Out: []byte(""), Err: nil},
-	})
-	// END TESTING
-}
-
 func setupApp() {
-	testMode()
+	// mocks.TestMode(&Docker)
 	MainApp = app.New()
 	MainWindow = MainApp.NewWindow("Hello World")
 	MainWindow.Resize(fyne.NewSize(400, 400))
--- src/utils/mocks/docker.go
+++ src/utils/mocks/docker.go
@@ -40,3 +40,17 @@ func PatchDockerForTesting(d *cmd.DockerClient) {
 		return cmd
 	}
 }
+
+func TestMode(docker *cmd.DockerClient) {
+	PatchDockerForTesting(docker)
+	AddCommandLines([]CommandStruct{
+		// List context
+		{Out: []byte(`[{"Current":true,"Description":"Current DOCKER_HOST based configuration","DockerEndpoint":"npipe:////./pipe/docker_engine","KubernetesEndpoint":"","ContextType":"moby","Name":"default","StackOrchestrator":"swarm"},{"Current":false,"Description":"","DockerEndpoint":"npipe:////./pipe/dockerDesktopLinuxEngine","KubernetesEndpoint":"","ContextType":"moby","Name":"desktop-linux","StackOrchestrator":""}]`), Err: nil},
+		// Set context
+		{Out: []byte(`[{"Current":true,"Description":"Current DOCKER_HOST based configuration","DockerEndpoint":"npipe:////./pipe/docker_engine","KubernetesEndpoint":"","ContextType":"moby","Name":"default","StackOrchestrator":"swarm"},{"Current":false,"Description":"","DockerEndpoint":"npipe:////./pipe/dockerDesktopLinuxEngine","KubernetesEndpoint":"","ContextType":"moby","Name":"desktop-linux","StackOrchestrator":""}]`), Err: nil},
+		// List services
+		{Out: []byte("36xvvwwauej0|~|frontend|~|replicated|~|5/5|~|nginx:alpine|~|80\n74nzcxxjv6fq|~|backend|~|replicated|~|3/3|~|redis:3.0.6|~|443\n"), Err: nil},
+		// Reset context
+		{Out: []byte(""), Err: nil},
+	})
+}
